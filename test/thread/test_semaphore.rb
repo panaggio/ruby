6e3ee4477475cdcf338d30b3e671487fab99b68b
Ricardo Panaggio <panaggio.ricardo@gmail.com>
2010-07-21 18:07:52 -0300

    Added reader_writer to Semaphore test

--- test/thread/test_semaphore.rb
+++ test/thread/test_semaphore.rb
@@ -3,14 +3,60 @@ require 'thread'
 
 class TestSemaphore < Test::Unit::TestCase
   def test_semaphore
-    grind(3, 5, Semaphore)
+    reader_writer(10, Semaphore)
+    producer_consumer(3, 5, Semaphore)
   end
 
   def test_thread_semaphore
-    grind(3, 5, Thread::Semaphore)
+    reader_writer(10, Thread::Semaphore)
+    producer_consumer(3, 5, Thread::Semaphore)
   end
 
-  def grind(buffer_size, data_count, klass, *args)
+  def reader_writer(num_msgs, klass, *args)
+    wsem = Semaphore.new
+    rsem = Semaphore.new
+    readcount = 0
+
+    stream = []
+    read_msgs = []
+    msgs = (1..num_msgs).to_a
+
+    reader = Thread.new do
+      while read_msgs.size < num_msgs
+        rsem.wait
+          readcount += 1
+          wsem.wait if readcount == 1
+        rsem.signal
+
+        read_msgs << stream.pop
+        p [msgs, stream, read_msgs]
+
+        rsem.wait
+          readcount -= 1
+          wsem.signal if readcount.zero?
+        rsem.signal
+      end
+    end
+
+    writer = Thread.new do
+      while msgs.size > 0
+        wsem.wait
+          stream << msgs.pop
+        p [msgs, stream, read_msgs]
+        wsem.signal
+      end
+    end
+
+    reader.join
+    writer.join
+
+    assert_equal stream.size, 0
+    assert_equal readcount, 0
+    assert_equal msgs.size, 0
+    assert_equal read_msgs.size, num_msgs
+  end
+
+  def producer_consumer(buffer_size, data_count, klass, *args)
     fillcount  = klass.new(0)
     emptycount = klass.new(buffer_size)
     pipe = []
